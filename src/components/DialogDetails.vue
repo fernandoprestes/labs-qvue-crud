1d914136b33492fe2ef6cec5abee06f46e508249
fernandoprestes <fernandoprestesc@gmail.com>
2022-12-02 09:54:40 -0300

    fix dialog

--- src/components/DialogDetails.vue
+++ src/components/DialogDetails.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { onMounted, reactive, ref, watch, toRef } from "vue";
+import { reactive, watch } from "vue";
 
 import postsService from "src/services/posts";
 
@@ -10,7 +10,6 @@ const props = defineProps<Props>();
 
 const { getById } = postsService();
 
-const dialog = ref(false);
 const data = reactive({
   course: {
     id: 0,
@@ -20,12 +19,6 @@ const data = reactive({
     description: "",
   },
 });
-
-const emit = defineEmits(["update:modelValue"]);
-const onCancel = () => {
-  dialog.value = false;
-  emit("update:modelValue", dialog.value);
-};
 
 watch(
   () => props.id,
@@ -37,7 +30,7 @@ watch(
 </script>
 
 <template>
-  <q-dialog v-model="dialog" persistent>
+  <q-dialog persistent>
     <q-card>
       <q-card-section class="items-center">
         <div class="text-h6">{{ data.course.title }}</div>
@@ -52,7 +45,7 @@ watch(
         </div>
       </q-card-section>
       <q-card-actions align="right">
-        <q-btn flat label="Cancel" color="primary" @click="onCancel" />
+        <q-btn flat label="Cancel" color="primary" v-close-popup />
       </q-card-actions>
     </q-card>
   </q-dialog>
